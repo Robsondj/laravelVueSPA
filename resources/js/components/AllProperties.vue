2a945338c84a2de46fdff492af78e49addd626ea
Robson de Jesus <robson.djsilva@gmail.com>
2020-07-12 15:29:25 -0300

    Added sort by columns Id and Email

--- resources/js/components/AllProperties.vue
+++ resources/js/components/AllProperties.vue
@@ -5,11 +5,11 @@
         <table id="propertyTable" class="table table-bordered">
             <thead>
             <tr class="bg-secondary text-white">
-                <th>Código</th>
-                <th>E-mail</th>
-                <th>Endereço</th>
-                <th>Status</th>
-                <th>Ações</th>
+                <th class="align-middle">Código <button class="btn btn-secondary" @click="sortById"><i class="fas fa-arrows-alt-v"></i></button></th>
+                <th class="align-middle">E-mail <button class="btn btn-secondary" @click="sortByEmail"><i class="fas fa-arrows-alt-v"></i></button></th>
+                <th class="align-middle">Endereço</th>
+                <th class="align-middle">Status</th>
+                <th class="align-middle">Ações</th>
             </tr>
             </thead>
             <tbody class="bg-light">
@@ -35,7 +35,9 @@
     export default {
         data() {
             return {
-                properties: []
+                properties: [],
+                sortId: 'asc',
+                sortEmail: 'asc',
             }
         },
         created() {
@@ -53,6 +55,24 @@
                         let i = this.properties.map(item => item.id).indexOf(id);
                         this.properties.splice(i, 1)
                     });
+            },
+            sortById() {
+                if (this.sortId === 'asc') {
+                    this.properties.sort((a,b) => a.id < b.id ? -1 : a.id > b.id ? 1 : 0)
+                    this.sortId = 'desc'
+                } else {
+                    this.properties.sort((a,b) => b.id < a.id ? -1 : b.id > a.id ? 1 : 0)
+                    this.sortId = 'asc'
+                }
+            },
+            sortByEmail() {
+                if (this.sortEmail === 'asc') {
+                    this.properties.sort((a,b) => a.owner_email < b.owner_email ? -1 : a.owner_email > b.owner_email ? 1 : 0)
+                    this.sortEmail = 'desc'
+                } else {
+                    this.properties.sort((a,b) => b.owner_email < a.owner_email ? -1 : b.owner_email > a.owner_email ? 1 : 0)
+                    this.sortEmail = 'asc'
+                }
             }
         }
     }
